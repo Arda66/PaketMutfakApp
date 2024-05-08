9d155c796c71a39e1305e4afeea968834a574783
Arda66 <ardaege66@hotmail.com>
2024-05-08 17:43:58 +0300

    improve colors a little bit.

--- src/screens/OrderDetailsScreen.tsx
+++ src/screens/OrderDetailsScreen.tsx
@@ -1,6 +1,5 @@
-// src/screens/OrderDetailScreen.tsx
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 interface OrderDetailScreenProps {
   route: any;
@@ -11,11 +10,15 @@ const OrderDetailScreen: React.FC<OrderDetailScreenProps> = ({route}) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.orderId}>Order ID: {order.id}</Text>
-      <Text style={styles.adressTitle}>Address:</Text>
-      <Text style={styles.address}> {order.address}</Text>
-      <Text style={styles.statusTitle}>Status: </Text>
-      <Text style={styles.status}>{order.status}</Text>
+      <View style={styles.card}>
+        <Text style={styles.header}>Order Details</Text>
+        <Text style={styles.label}>Order ID:</Text>
+        <Text style={[styles.value, styles.orderId]}>{order.id}</Text>
+        <Text style={styles.label}>Address:</Text>
+        <Text style={[styles.value, styles.address]}>{order.address}</Text>
+        <Text style={styles.label}>Status:</Text>
+        <Text style={styles.statusValue}>{order.status}</Text>
+      </View>
     </View>
   );
 };
@@ -23,62 +26,56 @@ const OrderDetailScreen: React.FC<OrderDetailScreenProps> = ({route}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    justifyContent: 'center',
     backgroundColor: '#f5f5f5',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  value: {
+    fontSize: 18,
+    marginBottom: 10,
+    color: '#333',
   },
   orderId: {
-    marginBottom: 20,
-    color: '#1a73e8',
-    fontSize: 24,
-    textAlign: 'center',
-    padding: 10,
-    backgroundColor: '#ffffff',
-    borderRadius: 5,
-    shadowColor: '#000',
-    shadowOffset: {width: 0, height: 2},
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  adressTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 10,
-    color: '#151a17',
+    color: '#1a73e8', // Blue color for Order ID
   },
   address: {
-    fontSize: 18,
-    color: '#236338',
-    marginBottom: 20,
-    backgroundColor: '#ffffff',
-    padding: 10,
-    borderRadius: 5,
+    color: '#236338', // Green color for Address
+  },
+  card: {
+    backgroundColor: 'white',
+    width: '90%',
+    padding: 20,
+    borderRadius: 10,
     shadowColor: '#000',
     shadowOffset: {width: 0, height: 2},
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
+    shadowOpacity: 0.2,
+    shadowRadius: 8,
     elevation: 5,
   },
-  statusTitle: {
-    fontSize: 20,
+  header: {
+    fontSize: 24,
+    color: '#333',
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textDecorationLine: 'underline',
+    textAlign: 'center',
+  },
+  label: {
+    fontSize: 18,
+    color: '#666',
+    fontWeight: '600',
+    marginTop: 10,
+  },
+  value: {
+    fontSize: 18,
+    marginBottom: 10,
+    color: '#333',
+  },
+  statusValue: {
+    fontSize: 18,
+    color: '#4CAF50',
     fontWeight: 'bold',
     marginBottom: 10,
-    color: '#151a17',
-  },
-  status: {
-    fontSize: 18,
-    color: '#333',
-    marginTop: 10,
-    fontWeight: 'bold',
-    backgroundColor: '#ffffff',
-    padding: 10,
-    borderRadius: 5,
-    shadowColor: '#000',
-    shadowOffset: {width: 0, height: 2},
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
   },
 });
 
