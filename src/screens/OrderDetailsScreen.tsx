1a4e48de4fe2fcaec27bfc1fc6a8af021c782b8f
Arda66 <ardaege66@hotmail.com>
2024-05-08 17:37:48 +0300

    Designed orderdetail screen

--- src/screens/OrderDetailsScreen.tsx
+++ src/screens/OrderDetailsScreen.tsx
@@ -1,19 +1,21 @@
 // src/screens/OrderDetailScreen.tsx
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
 interface OrderDetailScreenProps {
-  route: any; 
+  route: any;
 }
 
-const OrderDetailScreen: React.FC<OrderDetailScreenProps> = ({ route }) => {
-  const { order } = route.params;
+const OrderDetailScreen: React.FC<OrderDetailScreenProps> = ({route}) => {
+  const {order} = route.params;
 
   return (
     <View style={styles.container}>
-      <Text>Order ID: {order.id}</Text>
-      <Text>Address: {order.address}</Text>
-      <Button title="Update Status" onPress={() => console.log('Update Status')} />
+      <Text style={styles.orderId}>Order ID: {order.id}</Text>
+      <Text style={styles.adressTitle}>Address:</Text>
+      <Text style={styles.address}> {order.address}</Text>
+      <Text style={styles.statusTitle}>Status: </Text>
+      <Text style={styles.status}>{order.status}</Text>
     </View>
   );
 };
@@ -23,6 +25,60 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
     justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  orderId: {
+    marginBottom: 20,
+    color: '#1a73e8',
+    fontSize: 24,
+    textAlign: 'center',
+    padding: 10,
+    backgroundColor: '#ffffff',
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  adressTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#151a17',
+  },
+  address: {
+    fontSize: 18,
+    color: '#236338',
+    marginBottom: 20,
+    backgroundColor: '#ffffff',
+    padding: 10,
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  statusTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#151a17',
+  },
+  status: {
+    fontSize: 18,
+    color: '#333',
+    marginTop: 10,
+    fontWeight: 'bold',
+    backgroundColor: '#ffffff',
+    padding: 10,
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
 });
 
